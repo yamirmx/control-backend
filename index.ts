475b4ccf8fe482a229dc619108784e25d7d4e991
yamirmx <yamirmx2@gmail.com>
2026-06-05 10:00:08 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
